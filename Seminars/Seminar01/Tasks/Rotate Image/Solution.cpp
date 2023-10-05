88d074be74806ad83db7d09b931ac8031a116ddb
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-05 20:05:41 +0300

    Create Solution.cpp

---
+++ Seminars/Seminar01/Tasks/Rotate Image/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row=matrix.size(),col=matrix[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=i;j<col;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
