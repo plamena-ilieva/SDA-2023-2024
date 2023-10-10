73f2f78d597252876dbbaba1c8a323d217331546
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-10 21:52:04 +0300

    Create SelectionSort.cpp

---
+++ Algorithms/Sorting/SelectionSort.cpp
@@ -0,0 +1,12 @@
+void selectionSort(int* arr, size_t size) {
+  for (size_t i = 0; i < size - 1;	i++) {
+    size_t minElementIndex =	i;
+			
+    for (size_t j = i + 1; j	< size; j++)
+      if (arr[j] < arr[minElementIndex])
+        minElementIndex = j;
+
+    if (i != minElementIndex)
+      std::swap(arr[i],  arr[minElementIndex]);
+  }	
+}	
