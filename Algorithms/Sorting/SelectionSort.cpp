98762c3aa3cfda2e2ed7f8e33446c516d60f7085
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-10 21:52:40 +0300

    Update SelectionSort.cpp

--- Algorithms/Sorting/SelectionSort.cpp
+++ Algorithms/Sorting/SelectionSort.cpp
@@ -1,8 +1,8 @@
 void selectionSort(int* arr, size_t size) {
-  for (size_t i = 0; i < size - 1;	i++) {
-    size_t minElementIndex =	i;
+  for (size_t i = 0; i < size - 1; i++) {
+    size_t minElementIndex = i;
 			
-    for (size_t j = i + 1; j	< size; j++)
+    for (size_t j = i + 1; j < size; j++)
       if (arr[j] < arr[minElementIndex])
         minElementIndex = j;
 
