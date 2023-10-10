8f95a5c4f9dcf4eb00413b2d4ebbbf0985a54e47
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-11 00:02:44 +0300

    Create QuickSort.cpp

---
+++ Algorithms/Sorting/QuickSort.cpp
@@ -0,0 +1,30 @@
+size_t partition(int* arr, size_t size) {
+	int pivot = arr[size / 2];
+
+	int i = 0;
+	int j = size - 1;
+
+	while (true) {
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+
+		if (arr[i] == arr[j])
+			i++;
+
+		if (i < j)
+			std::swap(arr[i], arr[j]);
+		else
+			return j;
+	}
+}
+
+void quickSort(int* arr, size_t size) {
+    if (size < 2)
+        return;
+    size_t pivotIndex = partition(arr, size);       
+
+    quickSort(arr, pivotIndex);
+    quickSort(arr + pivotIndex + 1, size - pivotIndex - 1);
+}
