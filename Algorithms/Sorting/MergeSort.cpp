ee12b8741830706fd97532907487757c271757b9
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-11 14:15:15 +0300

    Update MergeSort.cpp

--- Algorithms/Sorting/MergeSort.cpp
+++ Algorithms/Sorting/MergeSort.cpp
@@ -1,35 +1,43 @@
-void merge(int* arr, size_t lenOne, int* arr2, size_t lenTwo) {
-	int* resultArray = new int[lenOne + lenTwo];
-
+void merge(int* arr, size_t lenOne, int* arr2, size_t lenTwo, int* buffer) {
 	int pntOne = 0;
 	int pntTwo = 0;
-	int resArrayIndex = 0;
+	int bufferIndex = 0;
 
 	while (pntOne < lenOne && pntTwo < lenTwo) {
 		if (arr[pntOne] <= arr2[pntTwo])
-			resultArray[resArrayIndex++] = arr[pntOne++];
+			buffer[bufferIndex++] = arr[pntOne++];
 		else
-			resultArray[resArrayIndex++] = arr2[pntTwo++];
+			buffer[bufferIndex++] = arr2[pntTwo++];
 	}
 
 	while (pntOne < lenOne)
-		resultArray[resArrayIndex++] = arr[pntOne++];
+		buffer[bufferIndex++] = arr[pntOne++];
 
 	while (pntTwo < lenTwo)
-		resultArray[resArrayIndex++] = arr2[pntTwo++];
+		buffer[bufferIndex++] = arr2[pntTwo++];
 
 	for (int i = 0; i < lenOne + lenTwo; i++)
-		arr[i] = resultArray[i];
-
-	delete[] resultArray;
+		arr[i] = buffer[i];
 }
 
-void mergeSort(int* arr, size_t size) {
+void mergeSortStep(int* arr, size_t size, int* buffer) {
     if(size < 2)
         return;
         
     size_t mid = size / 2;    
-    mergeSort(arr, mid);
-    mergeSort(arr + mid, size - mid);
-    merge(arr, mid, arr + mid, size - mid);
+    mergeSortStep(arr, mid, buffer);
+    mergeSortStep(arr + mid, size - mid, buffer);
+    merge(arr, mid, arr + mid, size - mid, buffer);
+}
+
+void mergeSort(int* arr, size_t size)
+{
+	if (!arr || size == 0)
+		return;
+
+	int* pBuffer = new int[size];
+
+	mergeSortStep(pArr, Size, pBuffer);
+
+	delete[]  pBuffer;
 }
