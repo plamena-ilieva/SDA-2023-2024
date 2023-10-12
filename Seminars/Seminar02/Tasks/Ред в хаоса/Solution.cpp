e1b7c8dd6240b1f5bc76e692c7480695a1d8495f
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-10-12 15:34:16 +0300

    Create Solution.cpp

---
+++ Seminars/Seminar02/Tasks/Ред в хаоса/Solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+const int MAX_NUM = 1e5;
+const int MAX_SIZE = 1e6;
+
+int arr[MAX_SIZE];
+int count[MAX_NUM]{};
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> arr[i];
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i < MAX_NUM; ++i)
+    {
+        count[i] += count[i - 1];
+    }
+
+    int *result = new int[n];
+    for (int i = n - 1; i >= 0; --i)
+    {
+        result[i] = --count[arr[i]];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << result[i] << " ";
+    }
+
+    return 0;
+}
