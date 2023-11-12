4c2361fe10772b2e076a88a1498d7a0eada968bc
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-11-12 23:14:26 +0200

    Update Solution.cpp

--- Seminars/Seminar06/Validate Binary Search Tree/Solution.cpp
+++ Seminars/Seminar06/Validate Binary Search Tree/Solution.cpp
@@ -1,61 +1,28 @@
-﻿#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <queue>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBSTRec(const TreeNode* root, long min, long max) {
+	    if (root == nullptr)
+		    return true;
 
-using namespace std;
+	    if (root->val <= min || root->val >= max)
+		    return false;
 
-struct Node {
-    vector<size_t> children;
-    size_t timeIn = 0;
-    size_t timeOut = 0;
+	    return isValidBSTRec(root->left, min, root->val) 
+            && isValidBSTRec(root->right, root->val, max);
+    }
+
+    bool isValidBST(const TreeNode* root) {
+        return isValidBSTRec(root, LONG_MIN, LONG_MAX);
+    }
 };
-
-const size_t MAX_SIZE = 1e6 + 1;
-Node tree[MAX_SIZE];
-size_t timeCounter = 1;
-
-void preOrder(size_t root) {
-    tree[root].timeIn = timeCounter++;
-
-    for (auto x: tree[root].children) {
-        preOrder(x);
-    }
-
-    tree[root].timeOut = timeCounter++;
-}
-
-bool isPredecessor(size_t p, size_t ch) {
-    return tree[p].timeIn < tree[ch].timeIn && tree[p].timeOut > tree[ch].timeOut;
-}
-
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    size_t N, parent, child;
-    cin >> N;
-    size_t ribs = N - 1;
-    cin >> parent >> child;
-    tree[parent].children.push_back(child);
-    size_t root = parent;
-    for (size_t i = 1; i < ribs; i++) {
-        cin >> parent >> child;
-        tree[parent].children.push_back(child);
-    }
-
-    preOrder(root);
-    size_t Q, p, ch;
-    cin >> Q;
-    for (size_t i = 0; i < Q; ++i) {
-        cin >> p >> ch;
-        if (isPredecessor(p,ch)) {
-            std::cout << "YES\n";
-        } else {
-            std::cout << "NO\n";
-        }
-    }
-
-    return 0;
-}
