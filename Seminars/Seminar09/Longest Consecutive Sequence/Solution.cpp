603655aace866c68eeab88a58283d1a0e3010680
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-12-12 09:30:36 +0200

    Update Solution.cpp

--- Seminars/Seminar09/Longest Consecutive Sequence/Solution.cpp
+++ Seminars/Seminar09/Longest Consecutive Sequence/Solution.cpp
@@ -1,23 +1,22 @@
-﻿class Solution {
+class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        if(nums.size()==0)
-        return 0;
-  int cnt  = 1;
-  int maxi= 1;
-       for(int i = 1; i<nums.size(); i++){
-           if(nums[i]!=nums[i-1]){
-               if(nums[i]-nums[i-1]==1){
-                   cnt++;
-               }
-               else{
-                   maxi= max(maxi,cnt);
-                   cnt=1;
-                   
-               }
-           }
-       }
-       return max(maxi,cnt);
+        unordered_set<int> set;
+        for(int num : nums){
+            set.insert(num);
+        }
+        int longestSequence = 0;
+        for(int num : nums){
+            if(set.find(num-1) == set.end()){
+                int currentNumber = num;
+                int currentSequence = 1;
+                while(set.find(currentNumber+1) != set.end()){
+                    currentNumber++;
+                    currentSequence++;
+                }
+                longestSequence = max(longestSequence, currentSequence);
+            }
+        }
+        return longestSequence;
     }
 };
